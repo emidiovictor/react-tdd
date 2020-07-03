a20957876f3377c702b73f2ffe8b732a86b0cc92
victor <victoremidio@yahoo.com.br>
2020-07-03 18:44:52 -0300

    feat: add authentication usecase

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,9 @@
+import { AccountModel } from './models/account-model'
+interface AuthenticationParams {
+  email: string
+  passoword: string
+}
+export interface Authentication {
+
+  auth(params: AuthenticationParams): Promise<AccountModel>
+}
