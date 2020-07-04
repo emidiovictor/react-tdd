745e0094900361cdbefc02d5455b398c16bc02c2
victor <victoremidio@yahoo.com.br>
2020-07-04 11:26:32 -0300

    feat: ensure remote authentication return accountmodel on succes

--- src/data/usecases/authentication/remote-autentication.ts
+++ src/data/usecases/authentication/remote-autentication.ts
@@ -1,18 +1,19 @@
 import { HttpPostClient } from '@/data/protocols/http/http-post-client'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { AuthenticationParams, Authentication } from '@/domain/usecases/authentication'
 import { InvalidCredencialsError } from '@/domain/models/errors/invalid-credencials-error'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { UnexpectedError } from '@/domain/models/errors/unexpeted-error'
+import { AccountModel } from '@/domain/models/account-model'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
-    private readonly httpPostclient: HttpPostClient) { }
+    private readonly httpPostclient: HttpPostClient<AuthenticationParams, AccountModel>) { }
 
-  async auth (params: AuthenticationParams): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
     const httpReponse = await this.httpPostclient.post({ url: this.url, body: params })
     switch (httpReponse.statusCode) {
-      case HttpStatusCode.okk: break
+      case HttpStatusCode.okk: return httpReponse.body
       case HttpStatusCode.unathorizerd: throw new InvalidCredencialsError()
       default: throw new UnexpectedError()
     }
--- src/domain/test/mock-authentication.ts
+++
@@ -1,6 +0,0 @@
-import { AuthenticationParams } from '@/domain/usecases/authentication'
-import faker from 'faker'
-export const mockAuthentication = (): AuthenticationParams => ({
-  email: faker.internet.email(),
-  passoword: faker.internet.password()
-})
