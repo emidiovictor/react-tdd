b0e213f900b8460676bd9906a30b1db6e62da6a7
victor <victoremidio@yahoo.com.br>
2020-07-06 15:01:31 -0300

    fix: ajust login header with correct html

--- src/presentation/components/login-header/login-header.tsx
+++ src/presentation/components/login-header/login-header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Logo from '../logo/logo'
 import Styles from './login-header.styles.scss'
 
 const LoginHeader: React.FC = () => {
   return (
-    <footer className={Styles.footer}></footer>
-
+    <header className={Styles.header}>
+      <Logo />
+      <h1>4Devs = Enquetes para devs</h1>
+    </header>
   )
 }
 
