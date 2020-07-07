44b2d33b2093380eed9f615e768cb9e1a0100af5
victor <victoremidio@yahoo.com.br>
2020-07-07 00:12:21 -0300

    test: ensure LoginStatus will not dislpay spinner and error

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Login from './login'
+describe('Login Component', () => {
+  test('should ', () => {
+    const { getByTestId } = render(<Login/>)
+    const errorWrap = getByTestId('error-wrap')
+    expect(errorWrap.childElementCount).toBe(0)
+  })
+})
