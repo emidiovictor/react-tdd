5f46cfbac76cb10d5b509f9fa2c822caa69a7551
victor <victoremidio@yahoo.com.br>
2020-07-07 14:31:17 -0300

    test: ensure Login calls validation with correct passowrd

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -46,10 +46,16 @@ describe('Login Component', () => {
     expect(passwordStatus.textContent).toBe('🔴')
   })
 
-  test('Should  call validation with correct value email', () => {
+  test('Should  call validation with correct  email', () => {
     const { sut,validationSpy } = makeSut()
     const emailInput = sut.getByTestId('email')
     fireEvent.input(emailInput, { target: { value: 'any_email' } })
     expect(validationSpy.input).toEqual({ email: 'any_email' })
+  })
+  test('Should  call validation with correct  password', () => {
+    const { sut,validationSpy } = makeSut()
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+    expect(validationSpy.input).toEqual({ password: 'any_password' })
   })
 })
