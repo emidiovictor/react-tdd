08fa072656ee57b5b97a1ab579db9642d776a4fe
victor <victoremidio@yahoo.com.br>
2020-07-07 08:45:48 -0300

    test: ensure login starts with login button disable

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import Login from './login'
 describe('Login Component', () => {
-  test('should ', () => {
+  test('Should start with initial state', () => {
     const { getByTestId } = render(<Login/>)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+
+    const submittButton = getByTestId('submit') as HTMLButtonElement
+    expect(submittButton.disabled).toBe(true)
   })
 })
