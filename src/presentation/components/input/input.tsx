2f8a67301d69cd876ddd047734e2026ae85f652f
victor <victoremidio@yahoo.com.br>
2020-07-09 18:03:34 -0300

    feat: ensure login shows valid state  if passowrd is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -9,11 +9,11 @@ const Input: React.FC<Props > = (props: Props) => {
   const error = state[`${props.name}Error`]
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '✅'
   }
 
   const getTitle = (): string => {
-    return error
+    return error ?? 'Tudo certo!'
   }
 
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
