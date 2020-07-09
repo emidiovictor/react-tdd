84dbf5ec374c81258fe6811a691cf26c51c3e2e3
victor <victoremidio@yahoo.com.br>
2020-07-09 17:26:42 -0300

    refactor: move valitionSpy to its own file and add faker

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,8 +1,8 @@
-import { IValidation } from '@/presentation/protocols/validation'
 import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react'
 import React from 'react'
 import Login from './login'
-
+import { ValidationSpy } from '@/presentation/test'
+import faker from 'faker'
 type SutTypes ={
   sut: RenderResult
   validationSpy: ValidationSpy
@@ -15,17 +15,6 @@ const makeSut = (): SutTypes => {
   return {
     sut,
     validationSpy
-  }
-}
-
-class ValidationSpy implements IValidation {
-  errorMessage: string
-  fieldName: string
-  fieldValue: string
-  validate (fieldName: string, fieldValue: string): string {
-    this.fieldName = fieldName
-    this.fieldValue = fieldValue
-    return this.errorMessage
   }
 }
 
@@ -49,17 +38,19 @@ describe('Login Component', () => {
   })
 
   test('Should  call validation with correct  email', () => {
+    const email = faker.internet.email()
     const { sut,validationSpy } = makeSut()
     const emailInput = sut.getByTestId('email')
-    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    fireEvent.input(emailInput, { target: { value: email } })
     expect(validationSpy.fieldName).toBe('email')
-    expect(validationSpy.fieldValue).toBe('any_email')
+    expect(validationSpy.fieldValue).toBe(email)
   })
   test('Should  call validation with correct  password', () => {
     const { sut,validationSpy } = makeSut()
+    const password = faker.internet.password()
     const passwordInput = sut.getByTestId('password')
-    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+    fireEvent.input(passwordInput, { target: { value: password } })
     expect(validationSpy.fieldName).toBe('password')
-    expect(validationSpy.fieldValue).toBe('any_password')
+    expect(validationSpy.fieldValue).toBe(password)
   })
 })
