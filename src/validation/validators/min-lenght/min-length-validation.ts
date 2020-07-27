bf55a837ce9c40168aa85cc6df69cbba56ff9661
Victor Emidio <victoremidio@yahoo.com.br>
2020-07-26 21:25:40 -0300

    test: ensure MinlengthValidation if valu is invalid

---
+++ src/validation/validators/min-lenght/min-length-validation.ts
@@ -0,0 +1,15 @@
+import { FieldValidation } from '@/validation/protocols/field-validation'
+import { InvalidFieldError } from '@/validation/errors'
+
+export class MinLengthValidation implements FieldValidation {
+  /**
+   *
+   */
+  constructor (readonly field: string, private readonly min: number) {
+
+  }
+
+  validate (value: string): Error {
+    return new InvalidFieldError()
+  }
+}
