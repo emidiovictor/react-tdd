9d54e6e65f356dfaed743ec3beaca547c33d7d7d
Victor Emidio <victoremidio@yahoo.com.br>
2020-07-26 22:23:13 -0300

    feat: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/index.tsx
@@ -0,0 +1,4 @@
+export * from './email/email-valition'
+export * from './min-lenght/min-length-validation'
+export * from './required-field/required-field-validation'
+export * from './validation-composite/validation-composite'
