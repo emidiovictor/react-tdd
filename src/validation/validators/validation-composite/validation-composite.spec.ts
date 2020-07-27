f29847c9151c931dfa4821908c70f0d6d91edf30
Victor Emidio <victoremidio@yahoo.com.br>
2020-07-26 21:57:06 -0300

    test: ensure ValidationComposite returns error if first validation fails

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,13 +4,15 @@ import { FieldValidationSpy } from '../test/mock-field-validation'
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('first_error')
+
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_message')
+    fieldValidationSpy2.error = new Error('seccond_error')
     const sut = new ValidationComposite([
       fieldValidationSpy,
       fieldValidationSpy2
     ])
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('any_message')
+    expect(error).toBe('first_error')
   })
 })
